bf3f081efb0bc9562d5a8920538c848b53c70b88
octavio <octaviosoto777@gmail.com>
2020-07-01 22:32:57 -0500

    Add image field to publisher model with custom upload

--- IndieGamesSite/publishers/models.py
+++ IndieGamesSite/publishers/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+def custom_pub_upload(instance, file_name):
+    if instance.pk:
+        old_instance = Publisher.objects.get(pk=instance.pk)
+        old_instance.image.delete()
+    return 'publisher/' + file_name
+
 # Create your models here.
 class Publisher(models.Model):
     def __str__(self):
@@ -13,3 +19,4 @@ class Publisher(models.Model):
     
     name = models.CharField(max_length=100, verbose_name='Nombre')
     texto = models.TextField(verbose_name='Texto del editor', blank=True, default='')
+    image = models.ImageField(blank=True, verbose_name="Imagen", upload_to=custom_pub_upload)
