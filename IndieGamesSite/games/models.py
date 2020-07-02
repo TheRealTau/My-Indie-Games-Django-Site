dc134da831271f5789faf32be1a969b6e815b90a
octavio <octaviosoto777@gmail.com>
2020-07-01 22:36:03 -0500

    Add Image fiel to game model and custom upload, adding too url field for steam link

--- IndieGamesSite/games/models.py
+++ IndieGamesSite/games/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from publishers.models import Publisher
 
+
+def custom_game_upload(instance, file_name):
+    if instance.pk:
+        old_instance = Game.objects.get(pk=instance.pk)
+        old_instance.image.delete()
+    return 'game/' + file_name
+
 # Create your models here.
 class Game(models.Model):
     def __str__(self):
@@ -14,3 +21,5 @@ class Game(models.Model):
     title = models.CharField(max_length=100, verbose_name='Titulo')
     publisher = models.ForeignKey(Publisher, max_length=100, verbose_name='Editor', on_delete=models.CASCADE, blank=True)
     release_date = models.DateField(verbose_name='Fecha de publicación')
+    image = models.ImageField(blank=True, verbose_name='Imagen', upload_to=custom_game_upload)
+    steam_link = models.URLField(blank=True, verbose_name='Link a pagina de steam')
