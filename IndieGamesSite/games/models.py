4944432afeeaea9ed7a143529cf4e9eb79ea46bc
octavio <octaviosoto777@gmail.com>
2020-07-06 15:05:52 -0500

    Added new custom upload on games model with file name pattern

--- IndieGamesSite/games/models.py
+++ IndieGamesSite/games/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from publishers.models import Publisher
+from functools import partial
 
 
-def custom_game_upload(instance, file_name):
+def custom_game_upload(instance, file_name, prefix):
     if instance.pk:
         old_instance = Game.objects.get(pk=instance.pk)
-        old_instance.image.delete()
-    return 'game/' + file_name
+        if prefix == 'cover':
+            old_instance.cover.delete()
+        elif prefix == 'preview':
+            old_instance.preview_picture.delete()
+    return "game/{0}_{1}_{2}".format(str(instance.pk), prefix, file_name)
 
 # Create your models here.
 class Game(models.Model):
