2e012357ebb6674243442ccc36ea7ef3d5bde18a
octavio <octaviosoto777@gmail.com>
2020-07-07 15:50:31 -0500

    Added game tags on views and added view for game list by tag

--- IndieGamesSite/games/urls.py
+++ IndieGamesSite/games/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import GamesListView, GameDetailView
+from .views import GamesListView, GameDetailView, TaggedListView
 
 
 urlpatterns = [
     path('', GamesListView.as_view(), name="games"),
     path('<int:pk>', GameDetailView.as_view(), name="game_detail"),
+    path('tagged/<slug:tag_name>', TaggedListView.as_view(), name="tagged_games"),
 ]
--- IndieGamesSite/games/views.py
+++ IndieGamesSite/games/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Game
+from taggit.models import Tag
 
 
 # Create your views here.
@@ -23,3 +24,18 @@ class GameDetailView(DetailView):
 
     def get_object(self):
         return get_object_or_404(Game, pk=self.kwargs['pk'])
+
+class TaggedListView(ListView):
+    model = Game
+    template_name = 'games/tagged_games_list.html'
+    paginate_by = 4
+    context_object_name = 'tagged_games'
+    # tag = None
+
+    def get_context_data(self, **kwargs):
+        tag = get_object_or_404(Tag, slug=self.kwargs['tag_name'].lower())
+        games = Game.objects.filter(tags=tag)
+        context = super().get_context_data(**kwargs)
+        context['games'] = games
+        context['tag'] = tag
+        return context
