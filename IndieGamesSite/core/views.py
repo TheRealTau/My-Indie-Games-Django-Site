dbb5c28f7bd1fb1b6fd850a9275f86a4260c8c3a
octavio <octaviosoto777@gmail.com>
2020-07-15 21:38:39 -0500

    Added carousel element on index template

--- IndieGamesSite/core/views.py
+++ IndieGamesSite/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from games.models import Game
+# from ..games.models import Game
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    recent_games = Game.objects.filter().order_by('pub_date')[:4]
+    context = {'recent_games': recent_games}
+    return render(request, 'core/index.html', context)
